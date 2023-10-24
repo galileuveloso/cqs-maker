34d54f6d88b852139b094c0312077c94cf811bc4
Galileu Frez Veloso <galileu.veloso@hotmail.com>
2023-10-23 21:09:16 -0300

    add props and rule

--- CQSMaker/CQSMaker/Maker.cs
+++ CQSMaker/CQSMaker/Maker.cs
@@ -1,4 +1,6 @@
-﻿namespace CQSMaker
+﻿using System.Text;
+
+namespace CQSMaker
 {
     public static class Maker
     {
@@ -11,49 +13,24 @@
             bool responseList
         )
         {
-            string result =
-@"using FluentValidation;
-using MediatR;
-using @project.Helpers;
+            bool command = IsCommand(type);
 
-namespace @project.Features.@featureFeature.@types
-{
-    public class @name@type : IRequest<@nameResponse>
-    {
+            StringBuilder result = new StringBuilder();
 
-    }
+            result.Append(GetUsings());
+            result.Append(GetNamespace(command));
+            result.Append(GetRequest(command));
+            result.Append(GetResponse());
+            result.Append(GetHandler());
+            result.Append(GetValidator(command));
 
-    public class @nameResponse
-    {
-
-    }
-
-    public class @nameHandler : IRequestHandler<@name@type, @nameResponse>
-    {
-        public async Task<@nameResponse> Handle
-            (
-                @name@type request,
-                CancellationToken cancellationToken
-            )
-            {
-                ArgumentNullException.ThrowIfNull
-                (
-                    request,
-                    MessageHelper.NullFor<@name@type>()
-                );
-
-                return new @nameResponse();
-            }
-    }
-
-    public class @name@typeValidator : AbstractValidator<@name@type>
-    {
-        public @name@typeValidator() 
-        {
-
-        }
-    }
-}";
+            //string result =
+            //    GetUsings() +
+            //    GetNamespace(command) +
+            //    GetRequest(command) +
+            //    GetResponse() +
+            //    GetHandler() +
+            //    GetValidator(command);
 
             if (responseList)
                 result =
@@ -66,7 +43,80 @@ namespace @project.Features.@featureFeature.@types
                 .Replace("@project", project)
                 .Replace("@type", type)
                 .Replace("@feature", feature)
-                .Replace(".Query", ".Queries");
+                .Replace(".Query", ".Querie")
+                .ToString();
         }
+
+        private static string GetUsings()
+            =>
+@"using FluentValidation;
+using MediatR;
+using @project.Helpers;";
+
+        private static string GetNamespace(bool command)
+            => @"
+
+namespace @project.Features.@featureFeature." + (command ? "Commands" : "Queries") + @"
+{";
+
+        private static string GetRequest(bool command)
+        {
+            return @"
+    public class @name@type : IRequest<@nameResponse>
+    {
+        " + (command ? "internal Guid UsuarioLogadoId { get; set; }" : "") + @"
+    }";
+        }
+
+
+        private static string GetResponse()
+            => @"
+
+    public class @nameResponse
+    {
+        public Guid Id { get; set; }
+    }";
+
+        private static string GetHandler()
+            => @"
+
+    public class @nameHandler : IRequestHandler<@name@type, @nameResponse>
+    {
+        public async Task<@nameResponse> Handle
+        (
+            @name@type request,
+            CancellationToken cancellationToken
+        )
+        {
+            ArgumentNullException.ThrowIfNull
+            (
+                request,
+                MessageHelper.NullFor<InserirCarroCommand>()
+            );
+
+            return new @nameResponse();
+        }
+    }";
+
+        private static string GetValidator(bool command)
+        {
+            return @"
+
+    public class @name@typeValidator : AbstractValidator<@name@type>
+    {
+        public @name@typeValidator()
+        { 
+            " + (command ?
+            @"RuleFor(item => item.UsuarioLogadoId)
+                .NotEmpty()
+                .WithMessage(MessageHelper.EmptyFor<@name@type>(item => item.UsuarioLogadoId));"
+            : "") + @"
+        }
+    }
+}";
+        }
+
+        private static bool IsCommand(string type)
+            => type.Equals("Command");
     }
 }
